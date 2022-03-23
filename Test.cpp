ecc5db37e4b279501a457b35eb27528138b224eb
Lior Abramovich <92924492+liorabramovich@users.noreply.github.com>
2022-03-23 16:22:18 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -10,22 +10,22 @@ Notebook notebook;
 
     TEST_CASE("write to written or deleted position"){
     //Trying to write on writen pos: horizontal and vertical
-    notebook.write(1,0,0,Direction::Horizontal,"lior");
-    CHECK_THROWS(notebook.write(1,0,0,Direction::Horizontal,"lior"));
-    CHECK_THROWS(notebook.write(1,0,0,Direction::Vertical,"lior"));
-    CHECK_THROWS(notebook.write(1,0,2,Direction::Vertical,"lior"));
-    CHECK_THROWS(notebook.write(1,0,3,Direction::Vertical,"lior"));
-    CHECK_THROWS(notebook.write(1,0,2,Direction::Horizontal,"lior"));
-    CHECK_THROWS(notebook.write(1,0,3,Direction::Horizontal,"lior"));
+    notebook.write(7,0,0,Direction::Horizontal,"lior");
+    CHECK_THROWS(notebook.write(7,0,0,Direction::Horizontal,"lior"));
+    CHECK_THROWS(notebook.write(7,0,0,Direction::Vertical,"lior"));
+    CHECK_THROWS(notebook.write(7,0,2,Direction::Vertical,"lior"));
+    CHECK_THROWS(notebook.write(7,0,3,Direction::Vertical,"lior"));
+    CHECK_THROWS(notebook.write(7,0,2,Direction::Horizontal,"lior"));
+    CHECK_THROWS(notebook.write(7,0,3,Direction::Horizontal,"lior"));
 
     //Trying to write on erased pos: horizontal and vertical
-    notebook.erase(1,0,0,Direction::Horizontal,5);
-    CHECK_THROWS(notebook.write(1,0,0,Direction::Horizontal,"lior"));
-    CHECK_THROWS(notebook.write(1,0,0,Direction::Vertical,"lior"));
-    CHECK_THROWS(notebook.write(1,0,2,Direction::Vertical,"lior"));
-    CHECK_THROWS(notebook.write(1,0,3,Direction::Vertical,"lior"));
-    CHECK_THROWS(notebook.write(1,0,2,Direction::Horizontal,"lior"));
-    CHECK_THROWS(notebook.write(1,0,3,Direction::Horizontal,"lior"));
+    notebook.erase(7,0,0,Direction::Horizontal,5);
+    CHECK_THROWS(notebook.write(7,0,0,Direction::Horizontal,"lior"));
+    CHECK_THROWS(notebook.write(7,0,0,Direction::Vertical,"lior"));
+    CHECK_THROWS(notebook.write(7,0,2,Direction::Vertical,"lior"));
+    CHECK_THROWS(notebook.write(7,0,3,Direction::Vertical,"lior"));
+    CHECK_THROWS(notebook.write(7,0,2,Direction::Horizontal,"lior"));
+    CHECK_THROWS(notebook.write(7,0,3,Direction::Horizontal,"lior"));
 }
 
 TEST_CASE("valid position") {
@@ -41,8 +41,7 @@ TEST_CASE("valid position") {
     CHECK_THROWS(notebook.write(50, 50, 101, Direction::Horizontal, "lior"));
 
     //read function
-    CHECK_THROWS(notebook.read(100, 99, 51, Direction::Horizontal, 3));
-    CHECK_THROWS(notebook.read(100, 99, 50, Direction::Horizontal, 3));
+
     CHECK_THROWS(notebook.read(-55, 85, -30, Direction::Horizontal, 3));
     CHECK_THROWS(notebook.read(65, -100, 51, Direction::Horizontal, -3));
     CHECK_THROWS(notebook.read(100, -99, -50, Direction::Horizontal, 3));
@@ -60,6 +59,18 @@ TEST_CASE("valid position") {
     CHECK_THROWS(notebook.erase(-100, 100, 50, Direction::Horizontal, 3));
     CHECK_THROWS(notebook.erase(50, 50, 101, Direction::Horizontal, 101));
     CHECK_THROWS(notebook.erase(50, 50, 101, Direction::Horizontal, 5));
+    
+    Notebook notebook2;
+    notebook2.write(0,0,0,Direction::Horizontal,"lior");
+    CHECK(notebook3.read(0,0,0,Direction::Horizontal,4)=="lior");
+    CHECK(notebook3.read(0,0,0,Direction::Horizontal,5)=="lior_");
+    CHECK(notebook3.read(0,0,1,Direction::Horizontal,3)=="ior");
+    CHECK(notebook3.read(0,0,1,Direction::Horizontal,5)=="_ior_");
+    CHECK(notebook3.read(2,2,2,Direction::Horizontal,4)=="____");
+
+
+
+
    }
     
 
